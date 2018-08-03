dd0135f71e0a42904dfe0951e845e6c73b9b5dad
Gunnar Bastkowski <gunnar@bastkowski.name>
2018-08-03 16:32:39 +0200

    Start some recursive implementation

--- src/main/scala/gbastkowski/kata/karatechop/BinarySearch.scala
+++ src/main/scala/gbastkowski/kata/karatechop/BinarySearch.scala
@@ -3,7 +3,17 @@ package gbastkowski.kata.karatechop
 object BinarySearch {
 
   def chop(value: Int, array: Array[Int]): Int = {
-    array.indexOf(value)
+    if (array.isEmpty) -1
+    else chop(value, array, 0, array.length - 1)
+  }
+
+  private[this] def chop(value: Int, array: Array[Int], from: Int, to: Int): Int = {
+    val idx = from + ((to - from) / 2)
+
+    if (array(idx) == value) idx
+    else if (from >= to) -1
+    else if (value < array(idx)) chop(value, array, from, idx)
+    else chop(value, array, idx, to)
   }
 
 }
